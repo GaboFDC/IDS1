1eaf32c6edf0dbe61f2fddf1119b6b0a1d835cb4
Gabo <gabo.fdc@gmail.com>
2016-09-03 13:09:06 -0500

    Agregada documentación de codido

--- ckan.py
+++ ckan.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
+# Importamos la API para conexiones con ckan
 from ckanapi import RemoteCKAN
 
+
+# Creamos la conexión con datahub 
 datahub = RemoteCKAN('https://datahub.io')
+
+# Creamos la conexión con publicdata  
 publicdata = RemoteCKAN('http://publicdata.eu')
 
+# Usamos la api para buscar todos los datasets con formato sparql
 raw_result = datahub.action.package_search(fq='res_format:api/sparql')
 
+# Imprimimos los resultados en bruto
 print(raw_result)
 
-print(len(raw_result['results']))
+# Imprimimos el titulo de cada uno de los resultados
 for res in raw_result['results']:
     print res['title']
